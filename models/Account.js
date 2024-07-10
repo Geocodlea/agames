0e239603f3b663e104e7343aac405e351c392cf4
Geocodlea <geocodlea@yahoo.com>
2024-07-10 15:14:35 +0300

    update Account model

--- models/Account.js
+++ models/Account.js
@@ -5,6 +5,14 @@ global.models = global.models || {};
 global.models.Account =
   global.models.Account ||
   mongoose.model("Account", {
+    provider: { type: String, required: true },
+    providerAccountId: { type: String, required: true, unique: true },
+    type: { type: String },
+    refresh_token: { type: String },
+    access_token: { type: String },
+    expires_at: { type: Number },
+    token_type: { type: String },
+    scope: { type: String },
     userId: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
