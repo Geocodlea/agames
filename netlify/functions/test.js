802134406561ef508b0be30fa8e322e19e078787
Geocodlea <geocodlea@yahoo.com>
2024-07-12 13:45:49 +0300

    9

--- netlify/functions/test.js
+++ netlify/functions/test.js
@@ -1,11 +1,40 @@
 import dbConnect from "../../utils/dbConnect";
 import User from "../../models/User";
+import nodemailer from "nodemailer";
 
-export default async (req) => {
-  console.log(req);
-
+export default async () => {
   await dbConnect();
-  const users = await User.find();
 
-  console.log(users);
+  const currentDate = new Date();
+
+  // Calculate one minute ago from the current time
+  const oneMinuteAgo = new Date(currentDate.getTime() - 1 * 60 * 1000);
+
+  const users = await User.find().select("lastActive email");
+
+  users.map(async (user) => {
+    // Compare the dates
+    if (user.lastActive > oneMinuteAgo) {
+      console.log("The date is within the last minute.");
+    } else {
+      console.log("The date is more than one minute old.");
+
+      // Create a transporter with your email service provider's details
+      const transporter = nodemailer.createTransport({
+        service: "Gmail",
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASS,
+        },
+      });
+
+      // Send the email
+      await transporter.sendMail({
+        from: process.env.EMAIL_FROM,
+        to: user.email,
+        subject: `Message from AGames - TEST`,
+        text: `TEST MAILLLLL`,
+      });
+    }
+  });
 };
