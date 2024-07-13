dbcdd71d2ecf3f7db93963dfccd80ebaf2801525
Geocodlea <geocodlea@yahoo.com>
2024-07-13 14:19:00 +0300

    16

--- netlify/functions/test.js
+++ netlify/functions/test.js
@@ -1,7 +1,7 @@
 import dbConnect from "../../utils/dbConnect";
 import User from "../../models/User";
 import nodemailer from "nodemailer";
-import { emailFooter } from "../../utils/emailFooter";
+import { emailFooter, emailFooterHtml } from "../../utils/emailFooter";
 
 export default async () => {
   await dbConnect();
@@ -54,25 +54,25 @@ export default async () => {
         // Send the email
         await transporter.sendMail({
           from: process.env.EMAIL_FROM,
-          to: user.email,
+          to: "geocodlea@yahoo.com",
           subject: "Important Notice: Your Account is at Risk of Deletion",
+          text: `Hi, \n\n We hope this message finds you well. \n\n Our records show that you haven’t logged into your account for over a year. To maintain our system’s efficiency and security, we periodically remove inactive accounts. \n\n Please be aware: If you do not log in within the next 30 days, your account will be permanently deleted. \n\n We value you as a user and would love to have you back. Please log in before ${deletionDate} to keep your account active: \n\n ${loginLink} \n\n Best regards, \n AGames Team \n\n P.S. If you no longer wish to keep your account, no action is required on your part. It will be automatically deleted after ${deletionDate}. ${emailFooter}`,
           html: `
-          <p>Hi,</p>
-          <p>We hope this message finds you well.</p>
-          <p>Our records show that you haven’t logged into your account for over a year. To maintain our system’s efficiency and security, we periodically remove inactive accounts.</p>
-          <p><strong>Please be aware: If you do not log in within the next 30 days, your account will be permanently deleted.</strong></p>
-          <p>We value you as a user and would love to have you back. Please log in before <strong>${deletionDate}</strong> to keep your account active:</p>
-          <p><a href="${loginLink}" style="text-decoration: none;"><button style="display: inline-block; padding: 10px 20px; font-size: 16px; color: white; background-color: #007bff; border: none; border-radius: 5px;">Log in to your account</button></a></p>
-          <p>If you have any questions or need assistance, our support team is here to help.</p>
-          <p>Best regards,</p>
-          <p><strong>[Your Company Name] Team</strong></p>
-          <p>P.S. If you no longer wish to keep your account, no action is required on your part. It will be automatically deleted after <strong>${deletionDate}</strong>.</p>
-          ${emailFooter}
-        `,
+                <p>Hi,</p>
+                <p>We hope this message finds you well.</p>
+                <p>Our records show that you haven’t logged into your account for over a year. To maintain our system’s efficiency and security, we periodically remove inactive accounts.</p>
+                <p><strong>Please be aware: If you do not log in within the next 30 days, your account will be permanently deleted.</strong></p>
+                <p>We value you as a user and would love to have you back. Please log in before <strong>${deletionDate}</strong> to keep your account active:</p>
+                <p><a href="${loginLink}" style="text-decoration: none; "><button style="display: inline-block; padding: 10px 20px; font-size: 16px; color: white; background-color: #007bff; border: none; border-radius: 5px; cursor: pointer;">Log in to your account</button></a></p>
+                <p>If you have any questions or need assistance, our support team is here to help.</p>
+                <p>Best regards,<br />
+                <strong>AGames Team</strong></p>
+                <p>P.S. If you no longer wish to keep your account, no action is required on your part. It will be automatically deleted after <strong>${deletionDate}</strong>.</p>
+                ${emailFooterHtml}
+              `,
         });
-        console.log(`Email sent to ${user.email}`);
       } catch (error) {
-        console.error(`Failed to send email to ${user.email}:`, error);
+        console.error(error);
       }
     }
   });
--- utils/emailFooter.js
+++ utils/emailFooter.js
@@ -1,2 +1,11 @@
 export const emailFooter =
-  "\r\n\r\n Ioana Mocanu \r\n Creative Board Gaming \r\n\r\n e: ioana.mocanu@cbgshop.ro \r\n t: 0736465213 \r\n w: www.cbgshop.ro / www.agames.ro \r\n f: www.facebook.com/CreativeBoardGaming";
+  "\n\n Ioana Mocanu \n Creative Board Gaming \n\n e: ioana.mocanu@cbgshop.ro \n t: 0736465213 \n w: www.cbgshop.ro / www.agames.ro \n f: www.facebook.com/CreativeBoardGaming";
+
+export const emailFooterHtml = `<p>
+  Ioana Mocanu<br>
+  Creative Board Gaming<br><br>
+  e: <a href="mailto:ioana.mocanu@cbgshop.ro">ioana.mocanu@cbgshop.ro</a><br>
+  t: <a href="tel:0736465213">0736465213</a><br>
+  w: <a href="http://www.cbgshop.ro">www.cbgshop.ro</a> / <a href="http://www.agames.ro">www.agames.ro</a><br>
+  f: <a href="http://www.facebook.com/CreativeBoardGaming">www.facebook.com/CreativeBoardGaming</a>
+</p>`;
