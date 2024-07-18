95a7c1f0ee9624d27595a40dd2593d3b10372448
Geocodlea <geocodlea@yahoo.com>
2024-07-18 21:09:03 +0300

    small fix

--- app/admin/[...id]/loading.js
+++ app/admin/[...id]/loading.js
@@ -1,15 +1,12 @@
-import { Box, Paper, Skeleton, Stack } from "@mui/material";
+import { Box, Paper, Skeleton, Stack, Typography } from "@mui/material";
 
 export default function Loading() {
   return (
     <Paper elevation={24} className="form-paper">
       <Stack spacing={2}>
-        <Skeleton
-          variant="rounded"
-          width="80%"
-          height={68}
-          style={{ marginBottom: "2rem", alignSelf: "center" }}
-        />
+        <Skeleton>
+          <Typography variant="h2">Update Event</Typography>
+        </Skeleton>
         <Skeleton variant="rounded" width="100%" height={56} />
         <Skeleton variant="rounded" swidth="100%" height={148} />
         <Skeleton variant="rounded" width="100%" height={56} />
