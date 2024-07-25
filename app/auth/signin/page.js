408f23615c94ec9dedaa2c7abd3f5316354c0e52
Geocodlea <geocodlea@yahoo.com>
2024-07-25 16:06:42 +0300

    signin session -> redirect to homepage

--- app/auth/signin/page.js
+++ app/auth/signin/page.js
@@ -1,8 +1,14 @@
+import { authOptions } from "/app/api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth/next";
+import { redirect } from "next/navigation";
 import { getProviders } from "next-auth/react";
 
 import Providers from "./Providers";
 
 export default async function Signin() {
+  const session = await getServerSession(authOptions);
+  if (session) redirect(`/`);
+
   const providers = await getProviders();
 
   return <Providers providers={providers} />;
